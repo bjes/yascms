4e507b63c5c005c92a2f7144b2b24c8cf24e615a
William Wu <william@pylabs.org>
2019-05-05 16:49:03 +0800

    調整樣板設定，使後續能支援上傳自訂樣板功能

--- tp_yass/views/home.py
+++ tp_yass/views/home.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/home.jinja2')
+@view_config(route_name='home', renderer='templates/default/home.jinja2')
 def home_view(request):
     return {'project': 'tp_yass'}
 
