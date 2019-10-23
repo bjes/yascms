9a4c2aa050a8fd3812467436b45d49bcd6d35e3a
William Wu <william@pylabs.org>
2019-10-23 11:21:17 +0800

    使用 f-strings 語法取代 format，並移除技術債

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -6,6 +6,7 @@ from invoke.exceptions import ParseError
 from tp_yass.tests.helper import get_ini_settings, import_test_db_data
 
 from . import db, test
+from .helper import find_ini_file
 
 
 @task(optional=['ini_file'])
@@ -13,16 +14,11 @@ def deploy(c, ini_file=None):
     """佈署此專案"""
 
     if ini_file is None:
-        if os.path.exists('production.ini'):
-            ini_file = 'production.ini'
-        elif os.path.exists('development.ini'):
-            ini_file = 'development.ini'
-        else:
-            raise ParseError('--ini-file 必須指定合法的 ini 設定檔')
+        ini_file = find_ini_file()
 
     c.run('git pull origin master')
     c.run('systemctl stop uwsgi')
-    c.run('alembic -c {} upgrade head'.format(ini_file))
+    c.run(f'alembic -c {ini_file} upgrade head')
     c.run('poetry install')
     c.run('systemctl start uwsgi')
 
--- tasks/db.py
+++ tasks/db.py
@@ -3,55 +3,70 @@ import re
 from invoke import Collection, task
 
 from tp_yass.tests.helper import get_ini_settings, import_test_db_data
+from .helper import find_ini_file
 
 
-@task(name='create')
-def db_create(c, ini_file):
+@task(name='create', optional=['ini_file'])
+def db_create(c, ini_file=None):
     """建立資料庫"""
+
+    if ini_file is None:
+        ini_file = find_ini_file()
+
     # Find database name via ini file
     sqlalchemy_url = get_ini_settings(ini_file)['sqlalchemy.url']
     db_name = re.findall(r'/(\w+)\?', sqlalchemy_url)[0]
     db_user, db_pass = re.findall(r'//(\w+):(\w+)@', sqlalchemy_url)[0]
-    c.run('sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS {} CHARSET utf8mb4"'.format(db_name))
-    c.run('sudo mysql -uroot -e "GRANT ALL ON {0}.* to {1}@localhost '
-          'IDENTIFIED BY \'{2}\'"'.format(db_name, db_user, db_pass))
+    c.run(f'sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS {db_name} '
+           'CHARSET utf8mb4"')
+    c.run(f'sudo mysql -uroot -e "CREATE USER {db_user}@localhost '
+          f'IDENTIFIED BY \'{db_pass}\'"')
+    c.run(f'sudo mysql -uroot -e "GRANT ALL ON {db_name}.* '
+          f'TO {db_user}@localhost')
 
 
-@task(name='delete')
-def db_delete(c, ini_file):
+@task(name='delete', optional=['ini_file'])
+def db_delete(c, ini_file=None):
     """刪除資料庫"""
+
+    if ini_file is None:
+        ini_file = find_ini_file()
+
     sqlalchemy_url = get_ini_settings(ini_file)['sqlalchemy.url']
     db_name = re.findall(r'/(\w+)\?', sqlalchemy_url)[0]
-    c.run('sudo mysql -uroot -e "DROP DATABASE IF EXISTS {}"'.format(db_name))
+    c.run(f'sudo mysql -uroot -e "DROP DATABASE IF EXISTS {db_name}"')
 
 
-@task(db_create, name='init')
-def init_db(c, ini_file):
+@task(db_create, name='init', optional=['ini_file'])
+def init_db(c, ini_file=None):
     """匯入初始資料至資料庫"""
-    c.run('alembic -c {} upgrade head'.format(ini_file))
-    c.run('initialize_tp_yass_db {}'.format(ini_file))
+
+    if ini_file is None:
+        ini_file = find_ini_file()
+
+    c.run(f'alembic -c {ini_file} upgrade head')
+    c.run(f'initialize_tp_yass_db {ini_file}')
 
 
-@task(db_delete, db_create, name='init-test')
-def init_test_db(c, ini_file):
+@task(db_delete, db_create, name='init-test', optional=['ini_file'])
+def init_test_db(c, ini_file=None):
     """匯入開發測試用的初始資料"""
-    c.run('alembic -c {} upgrade head'.format(ini_file))
+
+    if ini_file is None:
+        ini_file = find_ini_file()
+
+    c.run(f'alembic -c {ini_file} upgrade head')
     import_test_db_data(ini_file)
 
 
 @task(name='upgrade', optional=['ini_file'])
 def db_upgrade(c, ini_file=None):
     """將資料庫 migrate 至最新版"""
-
+    
     if ini_file is None:
-        if os.path.exists('production.ini'):
-            ini_file = 'production.ini'
-        elif os.path.exists('development.ini'):
-            ini_file = 'development.ini'
-        else:
-            raise ParseError('--ini-file 必須指定合法的 ini 設定檔')
+        ini_file = find_ini_file()
 
-    c.run('alembic -c {} upgrade head'.format(ini_file))
+    c.run(f'alembic -c {ini_file} upgrade head')
 
 
 @task(name='downgrade', optional=['ini_file'])
@@ -59,12 +74,7 @@ def db_downgrade(c, ini_file=None):
     """將資料庫 downgrade 至前一版"""
 
     if ini_file is None:
-        if os.path.exists('production.ini'):
-            ini_file = 'production.ini'
-        elif os.path.exists('development.ini'):
-            ini_file = 'development.ini'
-        else:
-            raise ParseError('--ini-file 必須指定合法的 ini 設定檔')
+        ini_file = find_ini_file()
 
-    c.run('alembic -c {} downgrade -1'.format(ini_file))
+    c.run(f'alembic -c {ini_file} downgrade -1')
 
