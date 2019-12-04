3b071132ded8a648d4524a19fe21169ae4254b23
William Wu <william@pylabs.org>
2019-12-04 09:46:03 +0800

    完成 login 畫面

--- tp_yass/routes.py
+++ tp_yass/routes.py
@@ -1,3 +1,4 @@
 def includeme(config):
     config.add_static_view('static', 'tp_yass:themes/default/static', cache_max_age=3600)
     config.add_route('index', '/')
+    config.add_route('login', '/login')
--- tp_yass/views/index.py
+++ tp_yass/views/index.py
@@ -4,3 +4,7 @@ from pyramid.view import view_config
 @view_config(route_name='index', renderer='themes/default/frontend/index.jinja2')
 def index_view(request):
     return {'project': 'tp_yass'}
+
+@view_config(route_name='login', renderer='themes/default/frontend/login.jinja2')
+def login_view(request):
+    return {}
