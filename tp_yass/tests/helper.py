cb3425d27fd742309a4047405d05dae9562daa7a
William Wu <william@pylabs.org>
2020-01-16 11:49:27 +0800

    完成帳號群組與最新消息的測試資料建置 helper

--- tp_yass/tests/helper.py
+++ tp_yass/tests/helper.py
@@ -11,20 +11,44 @@ def get_ini_settings(ini_file_path):
 
 def import_test_db_data(ini_file_path):
     """Import test data to test database"""
-
-    from mixer.backend.sqlalchemy import Mixer
+    from tp_yass.models.user import GroupModel, UserModel
+    from tp_yass.models.news import NewsModel
 
     ini_settings = get_ini_settings(ini_file_path)
     engine = engine_from_config(ini_settings)
     Session = sessionmaker(bind=engine)
-    mixer = Mixer(session=Session(), commit=True, locale='zh_TW')
 
-    # mixer.blend('tp_yass.models.mymodel', id=1, name='foo', value=1)
+    session = Session()
 
-    # with mixer.ctx(commit=False):
-    #     session = Session()
-    #     data = mixer.blend('tp_yass.models.mymodel',
-    #                        id=2, name='bar', value=2)
-    #     session.add(data)
-    #     session.commit()
+    # 建群組
+    group1 = GroupModel(id=1, name='測試國小', type=1)
+    group2 = GroupModel(id=2, name='教務處', type=1, ancestor_id=1)
+    group3 = GroupModel(id=3, name='自然領域科任', type=1, ancestor_id=1)
+    group4 = GroupModel(id=4, name='藝文領域科任', type=1, ancestor_id=1)
+    group5 = GroupModel(id=5, name='資訊組', type=1, ancestor_id=2)
+    group6 = GroupModel(id=6, name='系管師', type=1, ancestor_id=2)
 
+    # 建帳號
+    user1 = UserModel(id=1, first_name='陳', last_name='小明', email='user1@xxx.tp.edu.tw',
+                      account='user1', password='user1', status=1)
+    user1.groups = [group5, group4]
+    user2 = UserModel(id=2, first_name='王', last_name='大寶', email='user2@xxx.tp.edu.tw',
+                      account='user2', password='user2', status=1)
+    user2.groups = [group6, group3]
+
+    # 建最新消息
+    news1 = NewsModel(id=1, title='採購 10 台伺服器', content='設備已放機房', group_id=5)
+    news2 = NewsModel(id=2, title='暑假第一天將重灌電腦', content='請老師及早備份資料', group_id=6, is_pinned=1)
+
+    session.add(group1)
+    session.add(group2)
+    session.add(group3)
+    session.add(group4)
+    session.add(group5)
+    session.add(group6)
+    session.add(user1)
+    session.add(user2)
+    session.add(news1)
+    session.add(news2)
+
+    session.commit()
