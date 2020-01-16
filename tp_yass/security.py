e54073562dcf3d41b7a06fe281922655966343c7
William Wu <william@pylabs.org>
2020-01-16 10:57:43 +0800

    完成 groupfinder 邏輯定義

--- tp_yass/security.py
+++ tp_yass/security.py
@@ -1,10 +1,10 @@
-groups = {'admin': ['admin', 'manager'],
-          'johndoe': ['users']}
-
-
 def group_finder(user_name, request):
-    username = request.GET.get('username', None)
-    if username in groups:
-        return groups[username]
+    '''根據 session 存放的 group 資訊，找到登入的使用者所屬的 group id 列表'''
+
+    groups = request.session.get('groups', None)
+    if groups:
+        # groups 為一個多個 dict 組成的陣列，其中 key id 儲存了每個 group 的 id
+        # group name 可能會改，但 id 不會改，所以用 id 作為判斷依據
+        return [ i['id'] for i in groups ]
     else:
         return None
