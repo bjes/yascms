277a86a0281ce2f69d1a202e8baa770a89298812
William Wu <william@pylabs.org>
2020-01-16 16:57:34 +0800

    讓首頁顯示資料庫的最新消息

--- tp_yass/dal.py
+++ tp_yass/dal.py
@@ -3,12 +3,19 @@ import logging
 from pyramid_sqlalchemy import Session as DBSession
 
 from tp_yass.models.user import UserModel
+from tp_yass.models.news import NewsModel
 
 
 class DAL:
 
     @staticmethod
     def get_user(account, password):
+        '''根據傳入的帳號密碼找到對應的紀錄並回傳'''
         user = DBSession.query(UserModel).filter_by(account=account).one_or_none()
         if user and user.verify_password(password):
             return user
+
+    @staticmethod
+    def get_news_list():
+        '''傳回最新消息列表'''
+        return DBSession.query(NewsModel).order_by(is_pinned).order_by(NewsModel.id.desc())
--- tp_yass/views/frontend/index.py
+++ tp_yass/views/frontend/index.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
+from tp_yass.dal import DAL
 
 
 @view_config(route_name='index', renderer='themes/default/frontend/index.jinja2')
 def index_view(request):
-    return {'project': 'tp_yass'}
+    return {'news': DAL.get_news_list()}
