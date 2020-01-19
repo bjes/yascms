032751fb5660cbb131ca488f418220d809178e0c
William Wu <william@pylabs.org>
2020-01-19 15:57:41 +0800

    首頁 route name 更名為 homepage

--- tp_yass/routes.py
+++ tp_yass/routes.py
@@ -1,5 +1,7 @@
 def includeme(config):
     config.add_static_view('static', 'tp_yass:themes/default/static', cache_max_age=3600)
-    config.add_route('index', '/')
+
+    # frontend
+    config.add_route('homepage', '/')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
