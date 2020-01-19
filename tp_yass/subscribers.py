c960cff0e5c6d908e6cf8caf0af73990186bca30
William Wu <william@pylabs.org>
2020-01-19 13:11:52 +0800

    若網站進入唯讀模式則回傳 503 service unavailable

--- tp_yass/subscribers.py
+++ tp_yass/subscribers.py
@@ -1,8 +1,11 @@
 from pyramid.events import NewRequest
 from pyramid.events import subscriber
+from pyramid.httpexceptions import HTTPServiceUnavailable
 
 from tp_yass.dal import DAL
 
 @subscriber(NewRequest)
 def sysconfig(event):
     event.request.sysconfig = {config.name: config.value for config in DAL.get_sys_config()}
+    if event.request.sysconfig['maintenance_mode'] != 'false':
+        raise HTTPServiceUnavailable('唯讀模式啟用中。')
