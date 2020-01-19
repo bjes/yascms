6e5911617e614052c7997d9fd91657d4768fc4f8
William Wu <william@pylabs.org>
2020-01-19 12:12:16 +0800

    首頁顯示最新消息的筆數依據 sysconfig 設定決定

--- tp_yass/views/frontend/homepage.py
+++ tp_yass/views/frontend/homepage.py
@@ -4,4 +4,4 @@ from tp_yass.dal import DAL
 
 @view_config(route_name='index', renderer='themes/default/frontend/homepage.jinja2')
 def homepage_view(request):
-    return {'news': DAL.get_news_list()}
+    return {'news': DAL.get_news_list(request.sysconfig['homepage_news_quantity'])}
