d72df3050f9fafcbb7ea75e47b0c06c885f92656
William Wu <william@pylabs.org>
2020-01-19 12:06:22 +0800

    重構首頁樣板為 homepage

--- tp_yass/views/frontend/homepage.py
+++ tp_yass/views/frontend/homepage.py
@@ -2,6 +2,6 @@ from pyramid.view import view_config
 from tp_yass.dal import DAL
 
 
-@view_config(route_name='index', renderer='themes/default/frontend/index.jinja2')
+@view_config(route_name='index', renderer='themes/default/frontend/homepage.jinja2')
 def homepage_view(request):
     return {'news': DAL.get_news_list()}
