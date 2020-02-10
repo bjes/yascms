d489e7cd4cf5865a51f4fe82e00de4aa3cce4488
William Wu <william@pylabs.org>
2020-02-10 18:13:03 +0800

    navbar 加上 type 欄位

--- tp_yass/models/navbar.py
+++ tp_yass/models/navbar.py
@@ -18,7 +18,10 @@ class NavbarModel(BaseObject):
     name = Column(String(50), nullable=False, server_default='')
 
     # 連結的 url
-    link = Column(String(190), nullable=False, default='', server_default='')
+    link = Column(String(190), nullable=False, default='#', server_default='#')
+
+    # 選單類型， 1 為選單 （代表其下還有子選單， tree node），2 為連結 （leaf node），3 為分隔線 （dropdown divider)
+    type = Column(Integer, nullable=False)
 
     # 排序
     order = Column(Integer, nullable=False, default=0, server_default='0')
