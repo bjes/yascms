e4341031df6bbc38ba76b55ec1419e2d86522740
William Wu <william@pylabs.org>
2020-02-10 21:16:31 +0800

    修正 docstring coding style

--- tp_yass/dal.py
+++ tp_yass/dal.py
@@ -1,5 +1,3 @@
-import logging
-
 from pyramid_sqlalchemy import Session as DBSession
 
 from tp_yass.models.user import UserModel, GroupModel
@@ -11,19 +9,19 @@ class DAL:
 
     @staticmethod
     def get_user(account, password):
-        '''根據傳入的帳號密碼找到對應的紀錄並回傳'''
+        """根據傳入的帳號密碼找到對應的紀錄並回傳"""
         user = DBSession.query(UserModel).filter_by(account=account).one_or_none()
         if user and user.verify_password(password):
             return user
 
     @staticmethod
     def get_news_list(quantity):
-        '''傳回最新消息列表'''
+        """傳回最新消息列表"""
         return DBSession.query(NewsModel).order_by(NewsModel.is_pinned.desc()).order_by(NewsModel.id.desc())[:quantity]
 
     @staticmethod
     def get_sys_config():
-        '''傳回系統設定檔'''
+        """傳回系統設定檔"""
         return DBSession.query(SysConfigModel).all()
 
     @staticmethod
