6f69b92ce3b6f0d94cab42aa30015860293c77f0
William Wu <william@pylabs.org>
2020-02-14 13:23:36 +0800

    重構，將 backend 的 navbar 相關函式獨立至 helper

--- tp_yass/views/backend/navbar.py
+++ tp_yass/views/backend/navbar.py
@@ -1,46 +1,11 @@
 from pyramid.view import view_config
 
 from tp_yass.dal import DAL
-
-
-def _recursive_append(navbar_node, navbar):
-    if navbar.ancestor_id == navbar_node['id']:
-        navbar_node['descendants'].append({'id': navbar.id,
-                                           'name': navbar.name,
-                                           'url': navbar.url,
-                                           'is_external': navbar.is_external,
-                                           'icon': navbar.icon,
-                                           'type': navbar.type,
-                                           'module_name': navbar.module_name,
-                                           'order': navbar.order,
-                                           'descendants': []})
-        return True
-    else:
-        for descendant_navbar in navbar_node['descendants']:
-            _recursive_append(descendant_navbar, navbar)
+from tp_yass.views.helper.navbar import generate_navbar_trees
 
 
 @view_config(route_name='backend_navbar_list',
              renderer='themes/default/backend/navbar_list.jinja2',
              permission='view')
 def backend_navbar_list_view(request):
-    navbar_list = DAL.get_navbar_list()
-    navbar_trees = []
-    for navbar in navbar_list:
-        if not navbar.ancestor_id:
-            # 代表是最上層導覽列
-            navbar_trees.append({'id': navbar.id,
-                                 'name': navbar.name,
-                                 'url': navbar.url,
-                                 'is_external': navbar.is_external,
-                                 'icon': navbar.icon,
-                                 'type': navbar.type,
-                                 'module_name': navbar.module_name,
-                                 'order': navbar.order,
-                                 'descendants': []})
-        else:
-            # 代表是第二層以下的群組
-            for root_node in navbar_trees:
-                if _recursive_append(root_node, navbar):
-                    break
-    return {'navbar_trees': navbar_trees}
+    return {'navbar_trees': generate_navbar_trees(DAL.get_navbar_list())}
