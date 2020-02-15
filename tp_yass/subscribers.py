c975f4f67fdd0e6c750842dea133c03f4e9f8ea2
William Wu <william@pylabs.org>
2020-02-15 09:21:55 +0800

    重構：將 request.sysconfig 更名成 request.sys_config

--- tp_yass/subscribers.py
+++ tp_yass/subscribers.py
@@ -5,6 +5,6 @@ from tp_yass.dal import DAL
 
 
 @subscriber(NewRequest)
-def sysconfig(event):
-    """為避免不必要的權限檢查造成過多的資料庫存取，採用 event 在每次 request 時將 sysconfig 存入 request 下"""
-    event.request.sysconfig = {config.name: config.value for config in DAL.get_sys_config_list()}
+def sys_config(event):
+    """為避免不必要的權限檢查造成過多的資料庫存取，採用 event 在每次 request 時將 sys_config 存入 request 下"""
+    event.request.sys_config = {config.name: config.value for config in DAL.get_sys_config_list()}
--- tp_yass/views/frontend/homepage.py
+++ tp_yass/views/frontend/homepage.py
@@ -6,5 +6,5 @@ from tp_yass.dal import DAL
 
 @view_config(route_name='homepage', renderer='themes/default/frontend/homepage.jinja2')
 def homepage_view(request):
-    return {'news': DAL.get_latest_news(int(request.sysconfig['homepage_news_quantity'])),
+    return {'news': DAL.get_latest_news(int(request.sys_config['homepage_news_quantity'])),
             'navbar_trees': generate_navbar_trees(DAL.get_navbar_list())}
