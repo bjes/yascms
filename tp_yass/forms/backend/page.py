f0c42a465f42477af2aedc70b3c41a5fb7e0aff2
William Wu <william@pylabs.org>
2020-02-18 20:58:05 +0800

    新增 PageForm

---
+++ tp_yass/forms/backend/page.py
@@ -0,0 +1,20 @@
+from pyramid_wtforms import (Form,
+                             StringField,
+                             TextAreaField,
+                             MultipleFilesField)
+from pyramid_wtforms.validators import (InputRequired,
+                                        Length,
+                                        FileQuantity,
+                                        FileRequired,
+                                        FileSize)
+
+
+class PageForm(Form):
+    """單一網頁的編輯表單"""
+
+    title = StringField('標題', [InputRequired('此欄位必填'), Length(max=50, message='最長只接受 50 個字元')])
+
+    content = TextAreaField('內容')
+
+    # TODO: 要讓系統可以設定上傳的檔案大小限制，目前寫死必須小於 200 MB
+    attachments = MultipleFilesField('附件', [FileSize(max=200, base='mb')])
