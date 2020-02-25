2aba9c4070d99cccbd10cf4b4100becb51602886
William Wu <william@pylabs.org>
2020-02-26 02:39:58 +0800

    wording

--- tp_yass/forms/backend/page.py
+++ tp_yass/forms/backend/page.py
@@ -15,12 +15,12 @@ class PageForm(Form):
 
     content = TextAreaField('內容')
 
-    groups = StringField('群組*', [InputRequired('必須指定至少 1 個群組')])
+    groups = StringField('群組（以逗號分隔）*', [InputRequired('必須指定至少 1 個群組')])
 
     # TODO: 要讓系統可以設定上傳的檔案大小限制，目前寫死必須小於 200 MB
     attachments = MultipleFilesField('附件', [FileSize(max=200, base='mb')])
 
-    tags = StringField('標籤')
+    tags = StringField('標籤（以逗號分隔）')
 
 
 class PageEditForm(PageForm):
