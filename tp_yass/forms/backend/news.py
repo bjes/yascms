9d64c520fca4a22377bcaaad2ecf0063ef08e791
William Wu <william@pylabs.org>
2020-03-01 00:57:45 +0800

    強化最新消息建立表單檢驗邏輯

--- tp_yass/forms/backend/news.py
+++ tp_yass/forms/backend/news.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pyramid_wtforms import (Form,
                              StringField,
                              TextAreaField,
@@ -9,32 +11,45 @@ from pyramid_wtforms import (Form,
                              MultipleFilesField)
 from pyramid_wtforms.validators import (InputRequired,
                                         Length,
-                                        FileSize)
+                                        FileSize,
+                                        Optional,
+                                        ValidationError)
 
 
 class NewsForm(Form):
     """最新消息的建立表單"""
 
-    title = StringField('標題*', [InputRequired('此欄位必填'), Length(max=50, message='最長只接受 50 個字元')])
+    title = StringField('標題*', [InputRequired('標題欄位必填'), Length(max=50, message='最長只接受 50 個字元')])
 
-    content = TextAreaField('內容')
+    content = TextAreaField('內容*', [InputRequired('內容欄位必填')])
 
     # TODO: 要讓系統可以設定上傳的檔案大小限制，目前寫死必須小於 200 MB
     attachments = MultipleFilesField('附件', [FileSize(max=200, base='mb')])
 
     is_pinned = BooleanField('是否置頂')
 
-    pinned_start_date = DateField('置頂開始時間', format='%Y-%m-%d %H:%M')
+    pinned_start_date = DateField('置頂開始日期', format='%Y-%m-%d', validators=[Optional()])
 
-    pinned_end_date = DateField('置頂結束時間', format='%Y-%m-%d %H:%M')
+    pinned_end_date = DateField('置頂結束日期', format='%Y-%m-%d', validators=[Optional()])
 
-    visible_start_date = DateTimeField('顯示開始時間（未指定則代表馬上顯示）')
+    visible_start_date = DateTimeField('顯示開始時間（未指定則代表馬上顯示）',
+                                       format='%Y-%m-%d %H:%M',
+                                       validators=[Optional()])
 
-    visible_end_date = DateTimeField('顯示結束時間（未指定則代表永遠顯示）')
+    visible_end_date = DateTimeField('顯示結束時間（未指定則代表永遠顯示）',
+                                     format='%Y-%m-%d %H:%M',
+                                     validators=[Optional()])
 
     tags = StringField('標籤（以逗號分隔）')
 
     category_id = SelectField('分類群組', coerce=int)
+
+    def validate_is_pinned(form, field):
+        """若勾選了置頂，那麼置頂起訖日期都要設定"""
+        if field.data is True:
+            if not (isinstance(form.pinned_start_date.data, datetime)
+                    and isinstance(form.pinned_end_date.data, datetime)):
+                raise ValidationError('置頂起訖日期都需要指定')
 
     class Meta:
         locales = ['zh_TW', 'tw']
--- tp_yass/views/backend/news.py
+++ tp_yass/views/backend/news.py
@@ -17,3 +17,14 @@ class NewsCreateView:
         form = NewsForm()
         form.category_id.choices = [(category.id, category.name) for category in DAL.get_news_category_list()]
         return {'form': form}
+
+    @view_config(request_method='POST')
+    def post_view(self):
+        """處理建立最新消息的表單"""
+        form = NewsForm()
+        form.category_id.choices = [(category.id, category.name) for category in DAL.get_news_category_list()]
+        form.process(self.request.POST)
+        if form.validate():
+            import pdb; pdb.set_trace()
+        return {'form': form}
+
