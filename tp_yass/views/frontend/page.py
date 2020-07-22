fc8e1949344e23a5caf2524b884eaa4167159502
William Wu <william@pylabs.org>
2020-07-22 22:21:43 +0800

    修正前台 page 顯示時 navbar 沒有移除 root 的顯示錯誤

--- tp_yass/views/frontend/page.py
+++ tp_yass/views/frontend/page.py
@@ -8,4 +8,5 @@ from tp_yass.dal import DAL
 @view_config(route_name='page_get', renderer='tp_yass:themes/default/frontend/page_get.jinja2')
 def page_get_view(request):
     page = DAL.get_page(request.matchdict['page_id'])
-    return {'navbar_trees': generate_navbar_trees(generate_navbar_trees(request, visible_only=True)), 'page': page}
+    return {'navbar_trees': remove_navbar_root(generate_navbar_trees(request, visible_only=True)),
+            'page': page}
