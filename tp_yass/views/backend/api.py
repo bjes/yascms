711f1904cd2c261188884dd44129dfb4bc1462ee
William Wu <william@pylabs.org>
2020-07-23 02:02:20 +0800

    完成單個單一頁面的 api

--- tp_yass/views/backend/api.py
+++ tp_yass/views/backend/api.py
@@ -11,3 +11,13 @@ def backend_api_page_list(request):
                           'title': each_page.title,
                           'url': request.route_url('page_get', page_id=each_page.id)})
     return page_list
+
+
+@view_config(route_name='backend_api_page_get', renderer='json', permission='view')
+def backend_api_page_get(request):
+    """回傳單一頁面的 json"""
+    page = DAL.get_page(int(request.matchdict['page_id']))
+    if page:
+        return {'id': page.id, 'title': page.title, 'url': request.route_url('page_get', page_id=page.id)}
+    else:
+        return {}
