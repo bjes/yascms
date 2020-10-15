bf5e260a937d653ae96dada1fb3afd4ed5910a9c
William Wu <william@pylabs.org>
2020-10-16 03:10:13 +0800

    修復 test

--- tp_yass/tests/unit/test_homepage.py
+++ tp_yass/tests/unit/test_homepage.py
@@ -1,9 +1,15 @@
 from pyramid import testing
 
-from tp_yass.views.frontend.homepage import homepage_view
+from tp_yass.views.frontend import homepage
 
 
-def test_homepage_view():
-    info = homepage_view(testing.DummyRequest())
-    assert info == {'project': 'tp_yass'}
+def test_homepage_view(mocker):
+    request = testing.DummyRequest()
+    request.site_config = {'site_homepage_news_quantity': 10}
+    mocker.patch.object(homepage, 'DAL')
+    mocker.patch.object(homepage, 'remove_navbar_root')
+    mocker.patch.object(homepage, 'generate_navbar_trees')
+    info = homepage.homepage_view(request)
+    for key in ['NavbarType', 'link_list', 'navbar_trees', 'news_list', 'telext_list']:
+        assert key in info
 
