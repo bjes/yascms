3fcf19709234e5b7d835ee99dc15736add5256d9
William Wu <william@pylabs.org>
2020-12-02 15:15:05 +0800

    pip 降版

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ if [ "$1" != "project_only" ]; then
     apt install build-essential python3-dev python3-venv -y
 fi
 python3 -m venv .venv --system-site-packages
-.venv/bin/pip install 'pip==20.3' 'wheel==0.36.0' 'setuptools==50.3.2' 'ansible==2.10.4' 'poetry==1.1.4'
+.venv/bin/pip install 'pip==20.2.4' 'wheel==0.36.0' 'setuptools==50.3.2' 'ansible==2.10.4' 'poetry==1.1.4'
 .venv/bin/poetry run bash -c 'cd ansible && ./download_all_roles.sh'
 
 if [ "$1" == "project_only" ]; then
