614c279a09d86a50b8b08896cbe95af7de78a44c
William Wu <william@pylabs.org>
2020-12-02 14:09:51 +0800

    讓 venv 可以存取到 system site packages

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ fi
 if [ "$1" != "project_only" ]; then
     apt install build-essential python3-dev python3-venv -y
 fi
-python3 -m venv .venv
+python3 -m venv .venv --system-site-packages
 .venv/bin/pip install 'pip==20.3' 'wheel==0.36.0' 'setuptools==50.3.2' 'ansible==2.10.4' 'poetry==1.1.4'
 .venv/bin/poetry run bash -c 'cd ansible && ./download_all_roles.sh'
 .venv/bin/poetry run ansible-playbook ansible/deploy.yml --extra-vars="@ansible/deploy.conf"
