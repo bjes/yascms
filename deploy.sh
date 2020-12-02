28d13e18e73bb64bab8570042b5078c674924282
William Wu <william@pylabs.org>
2020-12-02 13:57:17 +0800

    補上 wheel 套件

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ if [ "$1" != "project_only" ]; then
     apt install build-essential python3-dev python3-venv -y
 fi
 python3 -m venv .venv
-.venv/bin/pip install 'pip==20.3' 'setuptools==50.3.2' 'ansible==2.10.4' 'poetry==1.1.4'
+.venv/bin/pip install 'pip==20.3' 'wheel==0.36.0' 'setuptools==50.3.2' 'ansible==2.10.4' 'poetry==1.1.4'
 .venv/bin/poetry run bash -c 'cd ansible && ./download_all_roles.sh'
 .venv/bin/poetry run ansible-playbook ansible/deploy.yml --extra-vars="@ansible/deploy.conf"
