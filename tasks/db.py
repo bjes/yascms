14ed3ac1f64fd969aa9ca522c78bfde52d29465b
William Wu <william@pylabs.org>
2020-12-28 14:31:13 +0800

    讓初始化資料庫的 task 可重複執行不出錯

--- tasks/db.py
+++ tasks/db.py
@@ -1,4 +1,5 @@
 import re
+import subprocess
 
 from invoke import Collection, task
 
@@ -44,9 +45,16 @@ def init_db(c, ini_file=None):
     if ini_file is None:
         ini_file = find_ini_file()
 
-    c.run(f'alembic -c {ini_file} upgrade head')
-    c.run(f'initialize_tp_yass_db {ini_file}')
+    # Find database name via ini file
+    sqlalchemy_url = get_ini_settings(ini_file)['sqlalchemy.url']
+    db_name = re.findall(r'@.+?/([^\?]+)', sqlalchemy_url)[0]
 
+    result = subprocess.run(['mysql', '-uroot', '-e', f'use { db_name }'], capture_output=True)
+    if result.returncode == 0:
+        print('資料庫已存在')
+    else:
+        c.run(f'alembic -c {ini_file} upgrade head')
+        c.run(f'initialize_tp_yass_db {ini_file}')
 
 @task(db_delete, db_create, init_db, name='init-test', optional=['ini_file'])
 def init_test_db(c, ini_file=None):
