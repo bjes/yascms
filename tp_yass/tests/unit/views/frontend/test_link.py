c29637ddfce79fe7729f2fc79724d1ddb84c1941
William Wu <william@pylabs.org>
2021-03-05 11:54:02 +0800

    完成 tpyass.views.frontend.link 測試

---
+++ tp_yass/tests/unit/views/frontend/test_link.py
@@ -0,0 +1,14 @@
+from pyramid.testing import DummyRequest
+
+from tp_yass.views.frontend import link
+
+
+def test_links_view_should_return_dict(mocker):
+    mocker.patch.object(link, 'generate_navbar_trees')
+    mocker.patch.object(link, 'remove_navbar_root')
+    mocker.patch.object(link.DAL, 'get_link_category_list')
+    response = link.links_view(DummyRequest())
+    assert isinstance(response, dict)
+    valid_keys = ['navbar_trees', 'NavbarType', 'link_category_list']
+    for key in valid_keys:
+        assert key in response
