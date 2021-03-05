45d1b9747ad3ab318f874abb9bde40cbb91c4c05
William Wu <william@pylabs.org>
2021-03-05 12:06:31 +0800

    修正 sanitize_input 遇到型態錯誤的行為

--- tp_yass/helper/__init__.py
+++ tp_yass/helper/__init__.py
@@ -11,5 +11,5 @@ def sanitize_input(param, param_type, default_value):
     """
     try:
         return param_type(param)
-    except ValueError:
+    except TypeError:
         return default_value
