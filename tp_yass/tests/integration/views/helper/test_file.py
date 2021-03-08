b77ffdd74576eaf9c4894d5737ee36287d596087
William Wu <william@pylabs.org>
2021-03-08 13:55:23 +0800

    修復失敗的 test

--- tp_yass/tests/integration/views/helper/test_file.py
+++ tp_yass/tests/integration/views/helper/test_file.py
@@ -2,7 +2,8 @@ from tp_yass.views.helper.file import convert_image_file
 
 
 def test_convert_image_file_should_convert_image_successfully(datadir, tmp_path):
+    original_img_name = 'logo.png'
     new_img_name = 'new_logo.png'
-    destination_path = tmp_path / 'new_logo.png'
-    assert convert_image_file(str(datadir / 'logo.png'), str(destination_path)) == new_img_name
+    destination_path = tmp_path / new_img_name
+    assert convert_image_file(str(datadir / original_img_name), str(destination_path)) == new_img_name
     assert destination_path.exists()
