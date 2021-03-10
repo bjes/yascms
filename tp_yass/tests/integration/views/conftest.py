7ca83941aca7a765412882f27c8a6e97c7f6edf8
William Wu <william@pylabs.org>
2021-03-10 09:10:49 +0800

    修正跑測試時若找不到 development.ini 的行為

--- tp_yass/tests/integration/views/conftest.py
+++ tp_yass/tests/integration/views/conftest.py
@@ -12,27 +12,37 @@ INI_FILE = HERE / 'development.ini'
 
 
 def get_global_config():
-    import collections
     """產生 tp_yass.main 所需的 global_config 資料結構"""
+    import collections
+
     global_config = collections.OrderedDict()
     global_config['here'] = str(HERE)
     global_config['__file__'] = str(INI_FILE)
     return global_config
 
 
-def get_settings():
-    """產生 tp_yass.main 所需的 settings 資料結構"""
+@pytest.fixture(scope='session')
+def ini_settings():
+    """產生 tp_yass.main 所需的 settings 資料結構。但取得之前必須確保 development.ini 有存在，所以做檢查，
+    若不存在，則用 soft link 建立測試用的檔案
+    """
     from tp_yass.tests.helper import get_ini_settings
+
+    if not INI_FILE.exists():
+        if INI_FILE.is_symlink():
+            INI_FILE.unlink()
+        test_ini_file = pathlib.Path(__file__).parent / 'data/test_development.ini'
+        INI_FILE.symlink_to(test_ini_file)
     return get_ini_settings(str(INI_FILE))
 
 
 @pytest.fixture
-def webtest_testapp(pyramid_config):
+def webtest_testapp(pyramid_config, ini_settings):
     """產生 webtest 物件以用來跑測試"""
     from webtest import TestApp
     from tp_yass import main
 
-    return TestApp(main(get_global_config(), **get_settings()))
+    return TestApp(main(get_global_config(), **ini_settings))
 
 
 @pytest.fixture
@@ -53,21 +63,9 @@ def init_test_db():
     subprocess.run(['inv', 'db.init-test'])
 
 
-@pytest.fixture(autouse=True)
-def init_ini_file(shared_datadir):
-    """在每次跑 integration tests 前檢查 development.ini 是否存在，
-    若否，用 soft link 建立測試用的檔案
-    """
-    if not INI_FILE.exists():
-        test_ini_file = shared_datadir / 'test_development.ini'
-        INI_FILE.symlink_to(test_ini_file)
-
-
 @pytest.fixture(autouse=True, scope='session')
-def init_sqlalchemy():
+def init_sqlalchemy(ini_settings):
     from sqlalchemy import create_engine
     from pyramid_sqlalchemy import init_sqlalchemy
 
-    ini_settings = get_settings()
     init_sqlalchemy(create_engine(ini_settings['sqlalchemy.url']))
-
