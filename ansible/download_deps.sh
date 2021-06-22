35f46d0db8c7aa96b0202f1bcedadc26c48ae5cb
William Wu <william@pylabs.org>
2021-06-22 11:44:31 +0800

    更新 ansible 語法，使用新版 module 命名寫法

--- ansible/download_deps.sh
+++ ansible/download_deps.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 rm -rf roles/*
-ansible-galaxy install -r requirements.yml -p roles/
+ansible-galaxy role install -r requirements.yml
+if [ -e collections/ansible_collections ]; then
+    rm -rf collections/ansible_collections/*
+fi
+ansible-galaxy collection install -r requirements.yml
